16c1297f9cfd155fc988408ace316ef1c91459ae
shibez <ginta2888@gmail.com>
2023-03-27 20:27:21 +0700

    feat: Add side bar and login view bar

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,12 +1,17 @@
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import { Outlet } from "react-router-dom";
+import { FaClipboardList, FaBloggerB } from "react-icons/fa";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
 import { Layout, Menu, theme } from "antd";
+import { ImBlog } from "react-icons/im";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 
 const MainLayout = () => {
@@ -14,35 +19,127 @@ const MainLayout = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const navigate = useNavigate();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-3 mb-0">
+            <span className="sm-logo">SB</span>
+            <span className="lg-logo">Shibez</span>
+          </h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["1"]}
+          onClick={({ key }) => {
+            if (key === "signout") {
+            } else {
+              navigate(key);
+            }
+          }}
           items={[
             {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashboard",
             },
             {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              key: "customers",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Customers",
             },
             {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
+              key: "catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Add Product",
+                },
+                {
+                  key: "product-list",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Product List",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                },
+                {
+                  key: "brand-list",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand List",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category",
+                },
+                {
+                  key: "category-list",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color",
+                },
+                {
+                  key: "color-list",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color List",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "blogs",
+              icon: <FaBloggerB className="fs-4" />,
+              label: "Blogs",
+              children: [
+                {
+                  key: "blog",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Add Blog",
+                },
+                {
+                  key: "blog-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog List",
+                },
+                {
+                  key: "blog-category",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Add Blog Category",
+                },
+                {
+                  key: "blog-category-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog Category List",
+                },
+              ],
+            },
+            {
+              key: "enquiries",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Enquiries",
             },
           ]}
         />
       </Sider>
       <Layout className="site-layout">
         <Header
+          className="d-flex justify-content-between ps-1 pe-5"
           style={{
             padding: 0,
             background: colorBgContainer,
@@ -55,6 +152,23 @@ const MainLayout = () => {
               onClick: () => setCollapsed(!collapsed),
             }
           )}
+          <div className="d-flex gap-3 align-items-center">
+            <div></div>
+            <div className="d-flex gap-3 align-items-center">
+              <div>
+                <img
+                  width={32}
+                  height={32}
+                  src="https://scontent-sin6-3.xx.fbcdn.net/v/t39.30808-6/335144950_666804928551801_8597803052702366422_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CrgfSTcgBdoAX_w2lkZ&_nc_ht=scontent-sin6-3.xx&oh=00_AfCuqMmfozMx3udK-qwWHoCOf6D0MwW9Zjupa_7Y9zOeJw&oe=6426CB23"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="mb-0">Shibez</h5>
+                <p className="mb-0">pqtoanhcmus@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </Header>
         <Content
           style={{
@@ -64,7 +178,7 @@ const MainLayout = () => {
             background: colorBgContainer,
           }}
         >
-          Content
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
