55d32134604302edccd13eb125773c5d3f0eb524
shibez <ginta2888@gmail.com>
2023-03-28 16:36:00 +0700

    feat: Add validator and form control for login using Formik and Yup

--- src/pages/CustomInput.js
+++ src/pages/CustomInput.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CustomInput = (props) => {
-    const {type, label, i_id, i_class} = props;
+  const { type, label, i_id, i_class, name, val, onCh } = props;
   return (
     <div className="form-floating mb-3">
       <input
@@ -9,6 +9,10 @@ const CustomInput = (props) => {
         className={`form-control ${i_class}`}
         id={i_id}
         placeholder={label}
+        name={name}
+        value={val}
+        onChange={onCh}
+        onBlur ={onCh}
       />
       <label htmlFor={label}>{label}</label>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,28 @@
 import React from "react";
 import CustomInput from "./CustomInput";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch, TypedUseSelectorHook } from "react-redux";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  let schema = Yup.object({
+    email: Yup.string()
+      .email("Email should be valid")
+      .required("Email is required"),
+    password: Yup.string().required("Email is required"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
       <br />
@@ -11,22 +31,47 @@ const Login = () => {
       <br />
       <br />
       <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
-        <form action="">
-          <h3 className="text-center title">Login</h3>
-          <p className="text-center">Login to your account to continue</p>
-          <CustomInput type="text" label="Email Address" id="email" />
-          <CustomInput type="password" label="Password" id="pass" />
-          <div className="mb-3 text-end">
-            <Link to="forgot-password" className="float-right">Forgot Password?</Link>
+        <h3 className="text-center title">Login</h3>
+        <p className="text-center">Login to your account to continue</p>
+        <form action="" onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            name="email"
+            label="Email Address"
+            id="email"
+            onCh={formik.handleChange("email")}
+            val={formik.values.email}
+          />
+          <div className="error">
+            {formik.touched.email && formik.errors.email ? (
+              <div>{formik.errors.email}</div>
+            ) : null}
           </div>
-          <Link
-            to={"/admin"}
+          <CustomInput
+            type="password"
+            name="password"
+            label="Password"
+            id="pass"
+            onCh={formik.handleChange("password")}
+            val={formik.values.password}
+          />
+          <div className="error">
+            {formik.touched.password && formik.errors.password ? (
+              <div>{formik.errors.password}</div>
+            ) : null}
+          </div>
+          <div className="mb-3 text-end">
+            <Link to="forgot-password" className="float-right">
+              Forgot Password?
+            </Link>
+          </div>
+          <button
             className="border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5"
             style={{ background: "#ffd333" }}
             type="submit"
           >
             Login
-          </Link>
+          </button>
         </form>
       </div>
     </div>
