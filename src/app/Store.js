fbff83cbcfecbf165f84405b7d07ec7376904ef4
shibez <ginta2888@gmail.com>
2023-03-29 17:12:06 +0700

    feat: View all users in customers table
    Send get request to /api/all-users and fetch data, then give data to table

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-
+import customerReducer from "../features/customers/customerSlice";
 export const Store = configureStore({
-    reducer: {auth: authReducer},
+    reducer: {auth: authReducer, customer: customerReducer},
 });
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,42 +1,56 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../features/customers/customerSlice";
 
 const Customers = () => {
-    const columns = [
-        {
-          title: "SNo",
-          dataIndex: "key",
-        },
-        {
-          title: "Name",
-          dataIndex: "name",
-        },
-        {
-          title: "Product",
-          dataIndex: "product",
-        },
-        {
-          title: "Status",
-          dataIndex: "status",
-        },
-      ];
-      const data1 = [];
-      for (let i = 0; i < 46; i++) {
-        data1.push({
-          key: i,
-          name: `Edward King ${i}`,
-          product: 32,
-          status: `London, Park Lane no. ${i}`,
-        });
-      }
-      return (
-        <div>
-          <h3 className="mb-4 title">Customers</h3>
-          <div>
-            <Table columns={columns} dataSource={data1} />
-          </div>
-        </div>
-      );
-}
+  const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.name.length - b.name.length,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Mobile",
+      dataIndex: "mobile",
+    },
+  ];
 
-export default Customers
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+  const customerState = useSelector((state) => state.customer.customers);
+  console.log(customerState);
+  const data = [];
+  for (let i = 0; i < customerState.length; i++) {
+    if (customerState[i].role !== "admin") {
+      data.push({
+        key: i + 1,
+        name: customerState[i].firstname + " " + customerState[i].lastname,
+        email: customerState[i].email,
+        mobile: customerState[i].mobile,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <h3 className="mb-4 title">Customers</h3>
+      <div>
+        <Table columns={columns} dataSource={data} />
+      </div>
+    </div>
+  );
+};
+
+export default Customers;
