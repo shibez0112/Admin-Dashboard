fd2e2be59450f57278f21af21d21bf3108541308
shibez <ginta2888@gmail.com>
2023-03-30 12:43:23 +0700

    feat: Intergrate API with brand list page

---
+++ src/features/brand/brandSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import brandService from "./brandService";
+
+export const getBrands = createAsyncThunk(
+  "brand/get-brands",
+  async (thunkAPI) => {
+    try {
+      return await brandService.getBrands();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+const initialState = {
+  brands: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+export const brandSlice = createSlice({
+  name: "brands",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBrands.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBrands.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(getBrands.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.brands = action.payload;
+      });
+  },
+});
+
+export default brandSlice.reducer;
--- src/pages/BrandList.js
+++ src/pages/BrandList.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from "antd";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getBrands } from '../features/brand/brandSlice';
 
 const BrandList = () => {
     const columns = [
@@ -8,32 +13,42 @@ const BrandList = () => {
           dataIndex: "key",
         },
         {
-          title: "Name",
-          dataIndex: "name",
+          title: "Title",
+          dataIndex: "title",
+          sorter: (a, b) => a.title.length - b.title.length,
         },
         {
-          title: "Product",
-          dataIndex: "product",
-        },
-        {
-          title: "Status",
-          dataIndex: "status",
+          title: "Action",
+          dataIndex: "action",
         },
       ];
-      const data1 = [];
-      for (let i = 0; i < 46; i++) {
-        data1.push({
-          key: i,
-          name: `Edward King ${i}`,
-          product: 32,
-          status: `London, Park Lane no. ${i}`,
+      const dispatch = useDispatch();
+      useEffect(() => {
+        dispatch(getBrands());
+      }, [dispatch]);
+      const brandState = useSelector((state) => state.brand.brands);
+      const data = [];
+      for (let i = 0; i < brandState.length; i++) {
+        data.push({
+          key: i + 1,
+          title: brandState[i].title,
+          action: (
+            <>
+              <Link className="fs-3 text-danger" to={"/"}>
+                <BiEdit />
+              </Link>
+              <Link className="ms-3 fs-3 text-danger" to={"/"}>
+                <AiFillDelete />
+              </Link>
+            </>
+          ),
         });
       }
       return (
         <div>
           <h3 className="mb-4 title">Brands</h3>
           <div>
-            <Table columns={columns} dataSource={data1} />
+            <Table columns={columns} dataSource={data} />
           </div>
         </div>
       );
