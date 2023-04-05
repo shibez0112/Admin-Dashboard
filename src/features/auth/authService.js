26726ecf2176407ec8d84e8bd8beef21e7f2f206
shibez <ginta2888@gmail.com>
2023-04-05 20:41:12 +0700

    fix: Enable using cookies header in request for authorization
    With the changes from https://github.com/shibez0112/Ecommerce/commit/24b0bda874c27823c19d2f93b273d0b1523a7be3 ,now every request make by axios can pass the middleware authentication at backend server using Cookie store in Application Cookies.
    
    resolve #1

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
 
+// make reference for commit
+axios.defaults.withCredentials = true;
+
 const login = async (userData) =>{
     const response = await axios.post(`${base_url}user/admin-login`, userData);
     if (response.data){
